53caabf76bc5ad3624e8969b8903401777276569
Nadine Brito <nadinecb2010@hotmail.com>
2023-09-26 20:48:47 -0400

    iniciando o projeto

--- Dificuldade Média/Mario Party/mario_party.py
+++ Dificuldade Média/Mario Party/mario_party.py
@@ -1,1 +1,23 @@
-print ("hello world")
+# Mario Party Game 
+
+# Importar Bibliotecas
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Instructions
+print ('Mario Party em Python')
+
+# Personagens: Mario, Luigi, Bowser, Peach, Daisy
+lista_de_personagens = ['Mario', 'Diddy Kong', 'Wa Luigi', 'Boom Boom' ]
+print ('Personagens:', lista_de_personagens)
+
+# Comece em uma base
+
+
+# Crie um caminho
+
+# Crie a linha de chegada
+
+# Crie a o dado
+
+# Determinar vencedores
